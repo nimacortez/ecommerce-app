1b15ea8ff82592df1b99d7b181d6795262d17e91
Nima H <nimahz@Nimas-MacBook-Air.local>
2023-05-29 19:14:39 -0400

    added product into shop

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { PRODUCTS } from "../../products";
+import { Product } from "./product";
 
 export const Shop = () => {
-  return <div>shop</div>
+  return (
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>Nima's Shop</h1>
+      </div>
+
+      <div className="products">
+        {PRODUCTS.map((product) => (
+          <Product data={product} />
+        ))}
+      </div>
+    </div>
+  );
 };
---
+++ src/products.js
@@ -0,0 +1,10 @@
+import product1 from "./assets/1.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 1499.99,
+    productImage: product1,
+  }
+];
