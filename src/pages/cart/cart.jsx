f083935b74fba6552d140fd3c8389cfff65ba49a
nimacortez <cortez.nima@gmail.com>
2023-06-22 18:17:59 -0400

    added navigate function for cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,6 +3,9 @@ import { PRODUCTS } from "../../products";
 import { ShopContext } from '../../context/shop-context';
 import { CartItem } from './cart-item';
 import "./cart.css";
+import { useNavigate} from 'react-router-dom';
+
+const navigate = useNavigate();
 
 export const Cart = () => {
   const { cartItems, getTotalCartAmount } = useContext(ShopContext); 
@@ -21,7 +24,7 @@ export const Cart = () => {
         })}
         <div className="checkout">
           <p> Subtotal: ${totalAmount}</p>
-          <button> Continue Shopping</button>
+          <button onClick={() => navigate("/")}> Continue Shopping</button>
           <button> Checkout</button>
           </div> 
     </div>
